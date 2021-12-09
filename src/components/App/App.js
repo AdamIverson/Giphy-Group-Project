a1981465beb00663209a4dec8a0df50e5567ea93
Adam Iverson <aciverso@gmail.com>
2021-12-09 14:50:48 -0600

    Routes up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes, Link } from "react-router-dom"
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 function App(props) {
   return (
@@ -7,14 +7,26 @@ function App(props) {
       <header>
         <h1>Hope U Like GIFs, Buddy</h1>
       </header>
-      <br>
-      <Router
-
-      <div>
-        <h1>Giphy Search!</h1>
-      </div>
+      <br></br>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+        </ul>
+      <Routes>
+        <Route exact path="/" element={FavoriteView}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/favorites" element={FavoritesView}/>
+      </Routes>
+      
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
