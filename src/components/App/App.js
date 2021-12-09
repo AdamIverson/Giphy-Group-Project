1301931c42dd13b4f5636a26532f7b46fe8c97ab
Adam Iverson <aciverso@gmail.com>
2021-12-09 13:50:35 -0600

    checklist update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom"
 
 function App(props) {
   return (
     <div>
-      <h1>Giphy Search!</h1>
+      <header>
+        <h1>Hope U Like GIFs, Buddy</h1>
+      </header>
+      <br>
+      <Router
+
+      <div>
+        <h1>Giphy Search!</h1>
+      </div>
     </div>
   );
 }
