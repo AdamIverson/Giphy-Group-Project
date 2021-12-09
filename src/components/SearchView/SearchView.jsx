46fc6ac18cd75bd1bc64273a0c4230af7629f06f
Bennett Smrdel <smrdelb@gmail.com>
2021-12-09 14:23:17 -0600

    set up index.js file and added a function text input

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -2,3 +2,39 @@ import React from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
+import { useState } from 'react';
+
+function SearchView() {
+    //Initial state is an OBJECT, with keys id and name
+    let [newGif, setGif] = useState({id: 1, name: ''});
+
+    // Declare dispatch and history
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    // Saves value of current text input
+    const handleNameChange = (event) => {
+        console.log('event happened');
+        //Similar to in redux -- we dont want to get rid of the id field when we update name
+        setGif({...newGif, name: event.target.value})
+    }
+
+    // Submits new gif object
+    const addNewGif = event => {
+        event.preventDefault();
+        dispatch({ type: 'POST_GIF', payload: newGif });
+        //updates the next Gif to have a new id
+        setGif({id:newGif.id + 1, name: ''});
+    }
+
+    return (
+        <form onSubmit={addNewGif}>
+            <input placeholder="Search for a gif, or don't" 
+            value={newGif.name}
+            onChange={handleNameChange} />
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default SearchView;
--- src/index.js
+++ src/index.js
@@ -8,4 +8,39 @@ import createSagaMiddleware from "redux-saga";
 import { put, takeEvery } from "redux-saga/effects";
 import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Saga GET route
+
+// Saga POST route
+
+// Saga DELETE route
+
+// Redux reducers
+// This is temporary, just to get the page to load
+const searchReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'POST_GIF':
+        return action.payload
+      default:
+        return state;
+    }
+  };
+
+// catcher function
+function* gifCatcher() {
+    
+}
+  
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+  
+const store = createStore(
+combineReducers({ 
+    searchReducer,
+}),
+applyMiddleware(sagaMiddleware, logger)
+);
+  
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(gifCatcher);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
