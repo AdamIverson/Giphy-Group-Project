fa0f3c97a64fa27c46a947c47935efce5a488d19
Bennett Smrdel <smrdelb@gmail.com>
2021-12-10 10:19:01 -0600

    created get route in search router

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const axios = require('axios');
 require('dotenv').config();
 
-
-
-
+router.get(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${req.query}`, (req, res) => {
+    yield put({
+        type: 'SET_RESULTS',
+        payload: response.data
+    });
+});
 
 module.exports = router;
